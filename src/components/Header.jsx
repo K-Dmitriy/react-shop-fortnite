34ba3e39fad8d44c9617a8badb122b6eb8a4d599
d.kozlov <d.kozlov@hottelecom.by>
2021-03-30 19:30:33 +0300

    start

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+function Header() {
+	return (
+		<nav className="green darken-1">
+			<div className="nav-wrapper">
+				<h1 className="brand-logo">React Shop</h1>
+				<ul id="nav-mobile" className="right hide-on-med-and-down">
+					<li>
+						<a
+							href="https://github.com/K-Dmitriy/react-shop-fortnite"
+							target="_blank"
+							rel="noreferrer"
+						>
+							Repo
+						</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	);
+}
+
+export default Header;
